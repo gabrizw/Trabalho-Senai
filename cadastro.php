da43bf21713b6abbb02bd85015d7990eed493ed5
Gabriel Bertaglia Valadares de Souza <gabrielbertaglia121@gmail.com>
2025-05-26 15:47:28 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $genero = $_POST['genero'];
+        $senha = $_POST['senha'];
+        if(in_array($email, $_SESSION['emails'])) {
+            echo "<script language='javascript' type='text/javascript'>
+                alert('E-mail já cadastrado!');
+                window.location.href='inicial.php'
+                </script>";
+            exit();
+        }
+        array_push($_SESSION['nomes'], $nome);
+        array_push($_SESSION['emails'], $email);
+        array_push($_SESSION['generos'], $genero);
+        array_push($_SESSION['senhas'], $senha);
+        header("Location: inicial.php");
+    }
+?>
